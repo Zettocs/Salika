14f351d30fff7b4fc9312bb582f0261603ad2dc4
Valentin Scottez <valentin.scottez@outlook.fr>
2024-01-09 15:57:26 +0100

    modifications

--- src/test/java/epsi/software/sakila/services/CityServiceTest.java
+++ src/test/java/epsi/software/sakila/services/CityServiceTest.java
@@ -1,5 +1,6 @@
 package epsi.software.sakila.services;
 
+import epsi.software.sakila.entities.City;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,10 +14,22 @@ class CityServiceTest {
     CityService cityService;
     @Test
     void read() {
+        cityService.read(1L);
     }
 
     @Test
     void readAll() {
         cityService.readAll().stream().forEach(c -> System.out.println(c));
+    }
+    @Test
+    void create() {
+        City city = new City();
+        city.setCity("Lille");
+    }
+
+    @Test
+    void update() {
+        City city = new City();
+        city.setCity("Lille");
     }
 }
