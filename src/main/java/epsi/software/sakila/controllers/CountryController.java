470448b9123d2e09f610c45f8ca9e7d3c584eacc
Valentin Scottez <valentin.scottez@outlook.fr>
2024-04-12 15:01:01 +0200

    first commit

---
+++ src/main/java/epsi/software/sakila/controllers/CountryController.java
@@ -0,0 +1,36 @@
+package epsi.software.sakila.controllers;
+
+import ch.qos.logback.classic.Logger;
+import epsi.software.sakila.entities.Country;
+import epsi.software.sakila.services.CountryService;
+import epsi.software.sakila.services.CountryServiceImpl;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/country")
+public class CountryController {
+
+    private static final Logger log = (Logger) LoggerFactory.getLogger(CountryController.class);
+    private CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping(path="/all")
+    public List<Country> readAll() {
+        return countryService.readAll();
+    }
+
+    @GetMapping(path="/id/{id}")
+    public Country read(@PathVariable Long id) {
+        return countryService.read(id);
+    }
+}
